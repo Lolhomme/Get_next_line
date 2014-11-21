5fd8f31ee4faf7f3b08389cd752afcb27e26d67d
Laulom anthony <anthonylaulom@gmail.com>
2014-11-21 14:04:26 +0100

    debut get

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaulom <anthonylaulom@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/19 15:38:23 by alaulom           #+#    #+#             */
+/*   Updated: 2014/11/21 10:30:17 by alaulom          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+int			get_next_line(int const fd, char **line);
+
+#define BUFF_SIZE 10
+
+#endif
