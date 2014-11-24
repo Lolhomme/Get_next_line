13431e611088edbc0af28d6cf33780e9d7e90879
Laulom anthony <anthonylaulom@gmail.com>
2014-11-24 17:35:46 +0100

    get

--- get_next_line.c
+++ get_next_line.c
@@ -6,20 +6,21 @@
 /*   By: alaulom <anthonylaulom@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/19 14:55:44 by alaulom           #+#    #+#             */
-/*   Updated: 2014/11/23 14:10:55 by alaulom          ###   ########.fr       */
+/*   Updated: 2014/11/24 12:12:39 by alaulom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <unistd.h>
 #include "get_next_line.h"
 
 int			get_next_line(int const fd, char **line)
 {
-	int				ret;
-	static int		end;
 	char			buffer[BUFF_SIZE];
 
-	read(fd, buffer, BUFF_SIZE);
-	if(!fd || !line || !(*line) || !BUFF_SIZE <= 0)
+	if (!fd || !line || !(*line) || !BUFF_SIZE <= 0)
 		return (-1);
+
+	read(fd, buffer, BUFF_SIZE);
+	*line = buffer;
 }
